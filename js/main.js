455924232e01fd9c58fa08f4c8b5b63fb4717690
k5va <k5va@yandex.ru>
2022-02-28 18:59:02 +0300

    Changed integer type to Number in jsDocs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * Returns random integer number between min and max inclusive.
- * @param {integer} min - min value inclusive
- * @param {integer} max - max value inclusive
+ * @param {Number} min - min value inclusive
+ * @param {Number} max - max value inclusive
  * @returns random integer number
  */
 const getRandomInteger = (min, max) => {
@@ -14,7 +14,7 @@ const getRandomInteger = (min, max) => {
 /**
  * Checks if string's length is less than or equal to max length
  * @param {string} str - string
- * @param {integer} maxLength - max length of the string
+ * @param {Number} maxLength - max length of the string
  * @returns true if string's length is less than or equal to max length
  */
 const checkStringLength = (str, maxLength) => str.length <= maxLength;
