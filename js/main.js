bf47103a2852e2e848624c8d4d6d475eb26dd349
k5va <k5va@yandex.ru>
2022-03-02 23:32:49 +0300

    Moved getRandomArrayElement to utils

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {getRandomInteger, uniqueId} from './utils/index.js';
+import {getRandomInteger, uniqueId, getRandomArrayElement} from './utils/index.js';
 
 const PHOTOS_COUNT = 25;
 
@@ -44,16 +44,6 @@ const CommentAvatarNumber = {
   MIN: 1,
   MAX: 6,
 };
-
-/**
- * Return random element from given array
- * @param {Array} elements - array of elements
- * @param {Boolean} remove - true if element should be deleted from array. False by default
- * @returns random element
- */
-const getRandomArrayElement = (elements, remove = false) => remove ?
-  elements.splice(getRandomInteger(0, elements.length - 1), 1)[0] :
-  elements[getRandomInteger(0, elements.length - 1)];
 
 const photoIdGenerator = uniqueId(1);
 const urlIdGenerator = uniqueId(1);
